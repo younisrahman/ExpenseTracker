db185e8e26aa8ffa9c5bdc0a79fea50b7fadb43a
Younis <younisrahman007@gmail.com>
2022-07-13 14:10:16 +0600

    add: Splash screen Add: Library

--- android/app/src/main/java/com/expensetracker/MainActivity.java
+++ android/app/src/main/java/com/expensetracker/MainActivity.java
@@ -3,6 +3,8 @@ package com.expensetracker;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ExpenseTracker";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 
   /**
